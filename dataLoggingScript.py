26a55ce7000da1792038093b60c698650e3a42f7
fayeefe <freija.orvokki@gmail.com>
2026-02-11 22:05:25 +0200

    v0.5
    Improved the data logging script

--- dataLoggingScript.py
+++ dataLoggingScript.py
@@ -1,13 +1,45 @@
 import csv
-data = []
+originalData = []
+VoltageData = []
+thicknessData = []
+
+feedRate = 0.3 #m/min
+
 with open("filename.csv", "r", newline='') as f:
     reader = csv.reader(f)
-    for row in f:
-        data.append(row)
-        print("test")
-print(data)
+    next(reader)
+    for row in reader:
+        data = []
+        voltage = float(3.3/1023*int(row[0]))
+        data.append(voltage)
+        data.append(row[1])
+        VoltageData.append(data)
+        data.clear
 
+        #Handles the thickness calculation !!!!!!!!!!!!!!the voltage values need to be calibrated with the machine!!!!!!!!!!!!!!!!!!!!!!
+        #thickness = float(12.7/(V_max-V_min)*voltage+v_offset)
+        #data.append(thickness)
+        #data.append(row[1])
+        #thicknessData.append(data)
+        originalData.append(row)
+
+# Print out the original values in a csv file (veeery unnecesary but a good template for file creation)
 with open('Testfile.csv', 'w', newline='') as csvFile:
-    # Fieldnames = ['Value', 'time']
+    fieldnames = ['Value', 'time']
     writer = csv.writer(csvFile)
-    writer.writerows(data)
+    writer.writerow(fieldnames)
+    writer.writerows(originalData)
+
+# creates the voltage values (not super necessary but good for debugging)
+with open('VoltageValues.csv', 'w', newline='') as Vcsv:
+    fieldnames = ['voltage', 'time']
+    writer = csv.writer(Vcsv)
+    writer.writerow(fieldnames)
+    writer.writerows(VoltageData)
+
+# creates the voltage values (not super necessary but good for debugging)
+with open('thicknessValues.csv', 'w', newline='') as Vcsv:
+    fieldnames = ['thickness', 'time', 'length']
+    writer = csv.writer(Vcsv)
+    writer.writerow(fieldnames)
+    writer.writerows(thicknessData)
