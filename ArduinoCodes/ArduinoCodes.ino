d9c73c7c29a9526fe4d1103b908d89908cfd2cd0
fayeefe <freija.orvokki@gmail.com>
2026-02-25 17:45:42 +0200

    Update ArduinoCodes.ino

--- ArduinoCodes/ArduinoCodes.ino
+++ ArduinoCodes/ArduinoCodes.ino
@@ -2,79 +2,114 @@
 #include <cmath>
 #include <iostream>
 
+
+using namespace std;
 const int buttonPin = 25;
+const int readingPin = 34; 
 hw_timer_t *timer = NULL;
 volatile uint32_t isrCounter = 0;
 volatile uint32_t lastIsrAt = 0;
 volatile SemaphoreHandle_t timerSemaphore ;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+deque<int> measurements;
+
+// takes the measurement when the semaphone is flipped
+
+void measurement(){
+  int dig = analogRead(readingPin);
+  measurements.push_front(dig);
+}
+
+int startTime;
+float average;
+// calculating the average value 
+void averaging(){
+  int addition = 0;
+  for(int i : measurements){
+    addition = addition + i;   
+  }
+  average = addition/3;
+  measurements.clear();
+  isrCounter = 1;
+  output()
+}
 
 void ARDUINO_ISR_ATTR onTimer(){ 
   portENTER_CRITICAL_ISR(&timerMux);
   isrCounter = isrCounter + 1; // increments the counter
   portEXIT_CRITICAL_ISR(&timerMux);
+
   xSemaphoreGiveFromISR(timerSemaphore, NULL);
-  Serial.write("timer tripped ");
-  Serial.write(isrCounter);
-}
-
-// takes the measurement when the semaphone is flipped
-std::deque<int> measurements;
-void measurement(){
-  int dig = digitalRead(34);
-  measurements.push_back(dig);
-  Serial.println("mea");
-}
-
-// calculating the average value and displaying it in serial monitor
-void averaging(){
-  int addition = 0;
-  for(int i : measurements){
-    addition = addition + i;
-    Serial.println("ave");
-
-  }
-  float average = addition/3;
-  measurements.pop_front();
-  measurements.pop_front();
-  Serial.println(average);
-  //Serial.println(millis());
 }
 
 void setup() {
-  pinMode(34, INPUT);
-  pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
+  pinMode(readingPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(115200);
   while(!Serial);
-  Serial.println("Connected");
   timerSemaphore = xSemaphoreCreateBinary(); //creates the semaphore
   timer = timerBegin(1000000);
   timerAttachInterrupt(timer, &onTimer);
-  timerAlarm(timer, 1000000, true, 0);
+  timerAlarm(timer, 500000, true, 0);
   timerStop(timer);
   timerWrite(timer,0);
-  //timerRestart(timer);
 }
-// voltage = 3.3/4096*dig;
+// displaying values in serial monitor
+void output(){
+  // voltage = 3.3/4096*dig;
       // float thickness1 = 12.7/(V_max-V_min)*3.3/4096*dig+v_offset) !!!!!! needs the voltage values from the actual device !!!!!!
       //int dig = analogRead(34);
+  int measurementTime = millis();
+  double takenTime = (measurementTime-startTime) /1000;
+  Serial.println(average); //!!!!!!!!!!!!!!! still the analogread value NOT thickness!!!!!!!!!!!!!!!!!!!
+  Serial.println(takenTime);
+}
+
+bool lastState = HIGH;
+bool latchedState = LOW;
 void loop() {
-  if(digitalRead(buttonPin) == HIGH && timerRead(timer) == 0){
+  bool currentState = digitalRead(buttonPin);
+  // latches the switch
+  if(currentState == LOW && lastState == HIGH){
+      delay(500);
     // turn on the timer here
+      latchedState = !latchedState;
+      Serial.println("button");
+    switch(latchedState){
+    case HIGH:
+      measurements.clear();
+      isrCounter = 0;
       timerStart(timer);
-    } /*else if(digitalRead(buttonPin) == HIGH && timerRead(timer) != 0){
+      break;
+    case LOW:
       timerStop(timer);
       timerWrite(timer,0);
-    }*/
-  
-
-  if(xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
-      if(float i = remainder(isrCounter,2) == 0){ //checks the remainder if it's an odd number or 0 it goes into 
-      measurement();
-    }else if (float i = remainder(isrCounter,2) != 0){
-      measurement();
-      averaging();
+      break;
     }
+  } 
+  lastState = currentState;
+  // changes the task according to the latched switch
+  
+  
+  if(xSemaphoreTake(timerSemaphore, 0) == pdTRUE){ 
+      switch(isrCounter){//checks the value of the isrcounter to see where in the cycle the system is
+        case 1:
+          measurement();
+          
+          break;
+        case 2:
+          measurement();         
+          break;
+        case 3:
+          measurement();
+          averaging();
+          startTime = millis();
+          measurement();
+          break;
+        default:
+          Serial.println("Error in isrCounter");
+          break;
+      }
   }
 }
 
